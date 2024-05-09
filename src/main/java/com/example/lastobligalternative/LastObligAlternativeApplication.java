9d4c26135dcfb0328374b42192622dbd753ce9ea
selmadyrlie <sejoh0292@oslomet.no>
2024-05-09 23:45:48 +0200

    Initial commit

---
+++ src/main/java/com/example/lastobligalternative/LastObligAlternativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lastobligalternative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastObligAlternativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastObligAlternativeApplication.class, args);
+    }
+
+}
